2da80f827e8469f56328edea87b553344affb4c5
gwiho <godj0311@handong.ac.kr>
2025-12-26 18:45:15 +0900

    first commit for server deploy test

---
+++ src/main/java/com/longkathon/LongkathonApplication.java
@@ -0,0 +1,13 @@
+package com.longkathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongkathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LongkathonApplication.class, args);
+    }
+
+}
